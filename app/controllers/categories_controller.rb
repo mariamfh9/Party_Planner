5bd492602312f0240bcf5a25d6c29e17c06d3520
mariamfh9 <mfh.haider@gmail.com>
2020-06-18 17:35:03 -0400

    fixed devise sign in options

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,10 +2,21 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+  
+  def new
+  end
 
+  def create
+  end
 
   def show
     @category = Category.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
   end
 
 
